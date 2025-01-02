76c793d89f159f7c6e5cc6634921cd0f5ef4c75a
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-01-02 18:39:31 +0530

    bugfix2

--- src/Components/PetProfile/PetProfile.tsx
+++ src/Components/PetProfile/PetProfile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams, useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import "./PetProfile.css";
 import rex from "../../assets/rex.jpg";
 
