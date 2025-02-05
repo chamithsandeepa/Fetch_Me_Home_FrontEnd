d00d73d3c2f7d15a1e7566e1558e7ef8e0103a9c
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-02-05 16:11:14 +0530

    Add types into adoptionform.ts file.

--- src/Components/AdoptionAcc/AdoptionAcc.tsx
+++ src/Components/AdoptionAcc/AdoptionAcc.tsx
@@ -2,25 +2,7 @@ import { useEffect, useState } from "react";
 import { Trash2, Edit } from "lucide-react";
 import axios from "axios";
 import "./AdoptionAcc.css";
-
-interface AdoptionForm {
-  id: string;
-  fullName: string;
-  telephone: string;
-  email: string;
-  address: string;
-  otherPetsDetails: string;
-  neuteredPets: string;
-  secureGarden: string;
-  animalSleepLocation: string;
-  workHours: string;
-  surrenderReason: string;
-  adoptionAgreement: string;
-  childrenUnder16: string;
-  homeOwnership: string;
-  leaseAllowsPets: string;
-  nearMainRoad: string;
-}
+import { AdoptionForm } from "../../types/adoptionform";
 
 const AdminPanel = () => {
   const [forms, setForms] = useState<AdoptionForm[]>([]);
---
+++ src/types/adoptionform.ts
@@ -0,0 +1,18 @@
+export interface AdoptionForm {
+  id: string;
+  fullName: string;
+  telephone: string;
+  email: string;
+  address: string;
+  otherPetsDetails: string;
+  neuteredPets: string;
+  secureGarden: string;
+  animalSleepLocation: string;
+  workHours: string;
+  surrenderReason: string;
+  adoptionAgreement: string;
+  childrenUnder16: string;
+  homeOwnership: string;
+  leaseAllowsPets: string;
+  nearMainRoad: string;
+}
