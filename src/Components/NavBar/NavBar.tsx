bd3d6c9ab01071209a0f87ef6cc38b8f71d4e009
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-02-05 19:39:06 +0530

    Change the logo

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import Fetch from "../../assets/Fetch.png";
 import "./NavBar.css";
 
 interface NavLink {
@@ -47,7 +48,7 @@ const NavBar: React.FC<NavBarProps> = ({ logoSrc, navLinks }) => {
       <div className="nav-container">
         <div className="logo-container">
           <Link to="/">
-            <img src={logoSrc} alt="Fetch Me Home" className="logo" />
+            <img src={Fetch} alt="Fetch Me Home" className="logo" />
           </Link>
         </div>
         <nav className="nav-links">
